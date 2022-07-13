f7a7f8302cc8284c36231c35a4484576e7bd00f4
TahaZairig <tahazairig1@gmail.com>
2022-07-13 19:36:49 +0100

    header completed

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 import '@popperjs/core'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/main.css'
+import './css/style.css'
 import './css/general-styles.css'
 import './css/responsive.css'
